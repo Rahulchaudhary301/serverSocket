227a1077aa634ae238e0832053a4b1cbd18a6409
Rahul Chaudhary <rahulchaudhary301@gmail.com>
2023-10-15 19:16:20 +0530

    done

---
+++ index.js
@@ -0,0 +1,67 @@
+// const app = require("express")();
+// const server = require("http").createServer(app);
+// const cors = require("cors");
+
+// const io = require("socket.io")(server, {
+// 	cors: {
+// 		origin: "*",
+// 		methods: [ "GET", "POST" ]
+// 	}
+// });
+
+// app.use(cors());
+
+// const PORT = process.env.PORT || 5000;
+
+// app.get('/', (req, res) => {
+// 	res.send('Running');
+// });
+
+
+// io.on("connection", (socket) => {
+// 	socket.emit("me", socket.id);
+
+// 	socket.on("disconnect", () => {
+// 		socket.broadcast.emit("callEnded")
+// 	});
+
+// 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+// 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+// 	});
+
+// 	socket.on("answerCall", (data) => {
+// 		io.to(data.to).emit("callAccepted", data.signal)
+// 	});
+// });
+
+// server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+
+const express = require("express")
+const http = require("http")
+const app = express()
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+})
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
+
+server.listen(5000, () => console.log("server is running on port 5000"))
