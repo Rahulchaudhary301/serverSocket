cda96018bd30193e6d09a8df2898dc16a1afddfa
Rahul Chaudhary <rahulchaudhary301@gmail.com>
2023-10-16 20:16:50 +0530

    done

--- index.js
+++ index.js
@@ -1,40 +1,3 @@
-// const app = require("express")();
-// const server = require("http").createServer(app);
-// const cors = require("cors");
-
-// const io = require("socket.io")(server, {
-// 	cors: {
-// 		origin: "*",
-// 		methods: [ "GET", "POST" ]
-// 	}
-// });
-
-// app.use(cors());
-
-// const PORT = process.env.PORT || 5000;
-
-// app.get('/', (req, res) => {
-// 	res.send('Running');
-// });
-
-
-// io.on("connection", (socket) => {
-// 	socket.emit("me", socket.id);
-
-// 	socket.on("disconnect", () => {
-// 		socket.broadcast.emit("callEnded")
-// 	});
-
-// 	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-// 		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-// 	});
-
-// 	socket.on("answerCall", (data) => {
-// 		io.to(data.to).emit("callAccepted", data.signal)
-// 	});
-// });
-
-// server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 
 const express = require("express")
@@ -50,6 +13,10 @@ const io = require("socket.io")(server, {
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id)
+
+	socket.on('callStatuss',(data)=>{
+		socket.broadcast.emit('callrecstatus',data)
+	})
 
 	socket.on("disconnect", () => {
 		socket.broadcast.emit("callEnded")
