e10b2de3de00d714ad87ab2239e4b0c11a2683c0
Rahul Chaudhary <rahulchaudhary301@gmail.com>
2023-10-17 19:46:09 +0530

    done

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		//origin: "https://rahulscreenvideo.netlify.app",
-		origin: "http://localhost:3000",
+		origin: "https://rahulscreenvideo.netlify.app",
+		//origin: "http://localhost:3000",
 		methods: [ "GET", "POST" ]
 	}
 })
